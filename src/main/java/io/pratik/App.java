42a0ac0ab40d95d88a3de00d2ed68a7079d669c7
Pratik Das <pratikd2000@gmail.com>
2021-04-18 09:27:43 +0400

    first commti

---
+++ src/main/java/io/pratik/App.java
@@ -0,0 +1,19 @@
+package io.pratik;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+// Read the full article https://dev.to/mateuszjarzyna/build-your-own-http-server-in-java-in-less-than-one-hour-only-get-method-2k02
+public class App {
+
+    public static void main( String[] args ) throws Exception {
+        System.out.println("Hello World");
+    }
+
+ 
+}
